69a2073c0a27619d2dc699628bc81653c3b5a5dd
MiaoQiu <1487981308@qq.com>
2024-06-26 21:49:49 +0800

    feat: 更新发送验证码接口的错误返回格式

--- service/user/user_send_confirm_email_service.go
+++ service/user/user_send_confirm_email_service.go
@@ -29,18 +29,18 @@ func getConfirmCode() string {
 func (service *UserSendConfirmEmailService) SendConfirmEmail() serializer.Response {
 	// check email format
 	if !utils.VerifyEmailFormat(service.UserEmail) {
-		return serializer.ParamsErr("email format err when send confirm email", nil)
+		return serializer.ParamsErr("NotEmail", nil)
 	}
 	// check user request email times in recent
 	if cache.RedisClient.Get(context.Background(), cache.RecentSendUserKey(service.UserEmail)).Val() != "" {
-		return serializer.ParamsErr("multi request send email", nil)
+		return serializer.ParamsErr("HasSendCode", nil)
 	}
 
 	// check if email has register
 	var emailNum int64
 	if err := model.DB.Model(&model.User{}).Where("user_name = ?", service.UserEmail).Count(&emailNum).Error; err != nil {
 		loglog.Log().Error("[UserSendConfirmEmailService.SendConfirmEmail] Fail to find user: ", err)
-		return serializer.DBErr("get user err", err)
+		return serializer.DBErr("", err)
 	}
 	if emailNum > 0 {
 		return serializer.ParamsErr("HasRegister", nil)
